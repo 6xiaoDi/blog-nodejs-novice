cb5343d3f49d912a271ebeb1f0878b2278e8c6d6
Mark Liu <ldtheended@163.com>
2020-09-19 01:36:40 +0800

    a0.04（example01-3——模块内导出 ）

--- Ma.js
+++ Ma.js
@@ -1,2 +1,14 @@
 console.log("我是Ma.js文件");
 let a = 10;
+class Person{
+	constructor(){
+		this.name = "张三";
+	}
+	hobby(){
+		console.log("喜欢篮球");
+	}
+}
+module.exports = {
+	a,
+	Person
+}
--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 console.log("我是index.js");
 // 引入
 let Ma = require("./Ma.js");
-console.log(a);
+console.log(Ma.a);
+let cai = new Ma.Person();
+cai.hobby();
