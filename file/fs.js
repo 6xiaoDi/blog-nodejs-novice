6046020d4f2ca6b8a3d4e0d57297804170b60630
6xd <ldtheended@163.com>
2020-09-22 00:00:07 +0800

    a0.14（文件操作——文件读取-`utf-8` 格式读取文件）

--- file/fs.js
+++ file/fs.js
@@ -1,8 +1,16 @@
 const fs = require("fs");
-fs.writeFile("1.txt","我是追加的文字",{flag:"a"},function(err){
+// fs.writeFile("1.txt","我是追加的文字",{flag:"a"},function(err){
+//     if(err){
+//         console.log(err);
+//     }else{
+//         console.log("添加成功");
+//     }
+// })
+
+fs.readFile("1.txt","utf-8",function(err,data){
     if(err){
         console.log(err);
     }else{
-        console.log("添加成功");
+        console.log(data)
     }
 })
