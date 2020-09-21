3061edbe58894b2bafb086d5cf318603831fb873
6xd <ldtheended@163.com>
2020-09-22 00:33:18 +0800

    a0.23（目录操作——修改目录名称）

--- file/fileSystem.js
+++ file/fileSystem.js
@@ -1,9 +1,17 @@
 const fs = require("fs");
 
-fs.mkdir("我的目录", err=>{
+// fs.mkdir("我的目录", err=>{
+//     if(err){
+//         console.log(err);
+//     }else{
+//         console.log("创建目录成功");
+//     }
+// })
+
+fs.rename("我的目录", "xd的目录", err=>{
     if(err){
         console.log(err);
     }else{
-        console.log("创建目录成功");
+        console.log("修改目录成功");
     }
-})
+});
