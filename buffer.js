0e6f74c99ff913e7fb2f804d7185bf636cb523b4
6xd <ldtheended@163.com>
2020-09-29 20:22:56 +0800

    a0.32（buffer操作—通过字符串创建）

--- buffer.js
+++ buffer.js
@@ -1,4 +1,7 @@
 // buffer创建
 // new Buffer() //nodejs6.0之前版本的创建格式
-let buffer = Buffer.alloc(10); // 指定大小（单位：字节）
+// let buffer = Buffer.alloc(10); // 指定大小（单位：字节）
+// console.log(buffer);
+
+let buffer = Buffer.from("大家好");
 console.log(buffer);
