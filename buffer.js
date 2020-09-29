9ad0edb70e80f82a4d4a33285da20975a8065477
6xd <ldtheended@163.com>
2020-09-29 20:27:14 +0800

    a0.34（buffer操作—中文乱码）

--- buffer.js
+++ buffer.js
@@ -6,5 +6,13 @@
 // let buffer = Buffer.from("大家好");
 // console.log(buffer);
 
-let buffer = Buffer.from([0xe5,0xa4,0xa7,0xe5,0xae,0xb6,0xe5,0xa5,0xbd]);
-console.log(buffer.toString());
+// let buffer = Buffer.from([0xe5,0xa4,0xa7,0xe5,0xae,0xb6,0xe5,0xa5,0xbd]);
+// console.log(buffer.toString());
+
+// 3个字节代表一个中文字符，这里4个字节打印出来会乱码
+let buffer1 = Buffer.from([0xe5,0xa4,0xa7,0xe5]);
+let buffer2 = Buffer.from([0xae,0xb6,0xe5,0xa5,0xbd]);
+console.log(buffer1.toString());
+// 连接两个buffer
+let newbuffer = Buffer.concat([buffer1,buffer2]);
+console.log(newbuffer.toString());
