4185c91390da879b37a4fb219058e57a8dfbef8b
6xd <ldtheended@163.com>
2020-09-29 20:24:56 +0800

    a0.33（buffer操作—通过数组创建）

--- buffer.js
+++ buffer.js
@@ -3,5 +3,8 @@
 // let buffer = Buffer.alloc(10); // 指定大小（单位：字节）
 // console.log(buffer);
 
-let buffer = Buffer.from("大家好");
-console.log(buffer);
+// let buffer = Buffer.from("大家好");
+// console.log(buffer);
+
+let buffer = Buffer.from([0xe5,0xa4,0xa7,0xe5,0xae,0xb6,0xe5,0xa5,0xbd]);
+console.log(buffer.toString());
