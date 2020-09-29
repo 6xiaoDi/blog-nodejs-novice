19c815a0fa873797a1851c083940eb8e34d0b5c0
6xd <ldtheended@163.com>
2020-09-29 23:14:28 +0800

    a0.38（stream流—stream流文件读取操作）

--- stream流.js
+++ stream流.js
@@ -1,4 +1,14 @@
 const fs = require("fs");
-let res = fs.readFileSync("1.txt");
+// let res = fs.readFileSync("1.txt");
 // console.log(res);
-console.log(res.toString());
+// console.log(res.toString());
+
+// 创建可读流
+let rs = fs.createReadStream("1.txt");
+
+// on监听事件，读取的数据在data事件中
+// 每次一小段一小段读取的chunk
+rs.on("data",chunk=>{
+    console.log(chunk);
+    console.log(chunk.toString());
+})
