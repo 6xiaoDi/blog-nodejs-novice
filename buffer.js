5924c51bfe31fdd3c3a8b3cee09c8b55e5b543bb
6xd <ldtheended@163.com>
2020-09-29 20:31:41 +0800

    a0.35（buffer操作—nodejs内置模块StringDecoder处理乱码，性能更好）

--- buffer.js
+++ buffer.js
@@ -14,5 +14,14 @@ let buffer1 = Buffer.from([0xe5,0xa4,0xa7,0xe5]);
 let buffer2 = Buffer.from([0xae,0xb6,0xe5,0xa5,0xbd]);
 console.log(buffer1.toString());
 // 连接两个buffer
-let newbuffer = Buffer.concat([buffer1,buffer2]);
-console.log(newbuffer.toString());
+// let newbuffer = Buffer.concat([buffer1,buffer2]);
+// console.log(newbuffer.toString());
+
+// 基于nodejs内置模块StringDecoder，更高一些的性能处理方式（处理乱码）
+let { StringDecoder } = require("string_decoder");
+let decoder =  new StringDecoder();
+let res1 = decoder.write(buffer1); // 自动处理乱码，存乱码并隐藏
+let res2 = decoder.write(buffer2);
+console.log(res1);
+console.log(res2);
+console.log(res1+res2);// 连接的时候会把隐藏的乱码加进去一块连接
