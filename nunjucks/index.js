7469bf4060218ab2d4082a139d37ad8d03c72c52
6xd <ldtheended@163.com>
2020-10-01 06:40:25 +0800

    a0.62（使用koa中的nunjucks）

--- nunjucks/index.js
+++ nunjucks/index.js
@@ -5,14 +5,15 @@ let app = new Koa();
 let router = new Router();
 // 使用nunjucks，首先需要配置
 app.use(nunjucks({
-    ext:"html", //后缀名设置，官方推荐后缀名.njk（方便区分）
+    ext:"html", //后缀名，官方推荐后缀名.njk（方便区分）
     path:__dirname+"/views", // 模版目录
     nunjucksConfig:{
         trimBlocks:true //防止Xss（以前常见的攻击网站方式，如在input框写script代码）漏铜；
     }
 }))
 router.get("/",async ctx=>{
-    ctx.body = "hello";
+    // ext已经设置后缀名了，这里就不需要重复再加后缀了
+    await ctx.render("index");
 })
 app.use(router.routes())
 app.listen(8000);
