74a36c490dfed4318249e38b1152cd42a73b193d
6xd <ldtheended@163.com>
2020-09-30 12:59:48 +0800

    a0.45（使用koa的pug模版引擎搭建服务）

---
+++ pug/index.js
@@ -0,0 +1,18 @@
+const Koa = require("koa");
+const Router = require("koa-router"); // 路由
+const views  = require("koa-views");  // 通过它来加载模版
+let app = new Koa();
+let router = new Router();
+// 将views（模版）加载进来，第一个参数：模版存放位置  第二个参数：配置选项，让其通过pug进行加载
+// __dirname 当前目录
+app.use(views(__dirname+"/views",{
+    map:{
+        html:"pug"
+    }
+}));
+// "/" 获取主页
+router.get("/",async ctx=>{
+    ctx.body = "hello";
+});
+app.use(router.routes());
+app.listen(3000);
