d2379828f99e5f1e9ef3113aafa8dc4ecbe86f25
6xd <ldtheended@163.com>
2020-09-30 11:25:37 +0800

    a0.42（stream流—stream流读取64kb文件切分为1块）

--- stream操作.js
+++ stream操作.js
@@ -1,14 +1,14 @@
 const fs = require("fs");
 // 创建一个65kb的文件；不写内容
-let buffer = Buffer.alloc(65*1024);
+// let buffer = Buffer.alloc(64*1024);
 // 第一个参数是文件名
-// fs.writeFile("64kb",buffer,err=>{
+// fs.writeFile("64kb(2)",buffer,err=>{
 //     if(err){
 //         return console.log(err);
 //     }
 //     console.log("写入成功");
 // })
-let rs = fs.createReadStream("64kb");
+let rs = fs.createReadStream("64kb(2)");
 // 每次一小段一小段读取的chunk
 // rs.on("data",chunk=>{
 //     console.log(chunk);
@@ -18,3 +18,4 @@ rs.on("data",chunk=>{
     num++;
     console.log(num);
 })
+
