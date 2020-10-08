2f604afd6d3b4d8c1562146be1874e04bf43d104
6xd <ldtheended@163.com>
2020-10-08 11:47:10 +0800

    a0.78（原生node如何区分路由？—— 页面乱码问题）

--- native node/index.js
+++ native node/index.js
@@ -1,12 +1,14 @@
 // 创建服务器
-// 引入原生http模块
 const http = require("http");
-// req 浏览器到node端 res 返还相关（服务端给浏览器的）
+// req 客户端到node端 res 返还相关（服务端给浏览器的）
 let server = http.createServer((req,res)=>{
     console.log(req.url);
-    res.write("hello client");
+    if(req.url==="/index"){
+        res.write("主页");
+    }else if (req.url==="/detail"){
+        res.write("详细页面");
+    }
     res.end();
 })
 // 端口一般设置4位或者4位以上的，因为系统端口都是4位以下的
 server.listen(4000);
-
