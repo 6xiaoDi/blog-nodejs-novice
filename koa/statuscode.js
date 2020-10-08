ab6dc7022e990bc32f3ff9f89bfe9b8454f0a385
6xd <ldtheended@163.com>
2020-10-08 16:08:08 +0800

    a0.96（302状态码）

--- koa/statuscode.js
+++ koa/statuscode.js
@@ -1,7 +1,10 @@
 const Koa = require("koa");
 const app = new Koa();
 app.use(ctx=>{
-    ctx.status = 404;
+    // ctx.status = 404;
+    ctx.status = 302; //临时重定向；然后去找头部location
+    //设置头部location，如果不设置就不会跳转
+    ctx.set("location","http://www.baidu.com");
     ctx.body = "hello";
 });
 app.listen(4000);
