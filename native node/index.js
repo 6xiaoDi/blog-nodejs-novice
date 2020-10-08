531fa2c6fd09ffd944f9415695393d6c903d90ca
6xd <ldtheended@163.com>
2020-10-08 10:42:34 +0800

    a0.76（原生node如何区分路由？—— 服务搭建）

---
+++ native node/index.js
@@ -0,0 +1,11 @@
+// 创建服务器
+// 引入原生http模块
+const http = require("http");
+// req 浏览器到node端 res 返还相关（服务端给浏览器的）
+let server = http.createServer((req,res)=>{
+    res.write("hello client");
+    res.end();
+})
+// 端口一般设置4位或者4位以上的，因为系统端口都是4位以下的
+server.listen(4000);
+
