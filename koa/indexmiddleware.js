469e778070700d3ede9ab80965933225e8356fe2
6xd <ldtheended@163.com>
2020-10-08 15:32:36 +0800

    a0.87（app.use使用——多个中间件的执行顺序，控制权逐层转交。）

---
+++ koa/indexmiddleware.js
@@ -0,0 +1,23 @@
+// koa  express 是nodejs的框架；轻量级；
+// 创建服务器；
+const Koa = require("koa");
+let app = new Koa();  //Application
+// 中间件：理解为函数；
+// ctx:context（上下文）简写；（相当于原生res和req的集合体或封装体）；
+// next 将控制权转交给下一个中间件
+let m1 = function(ctx,next){
+    console.log("m1start");
+    next();  //转交控制权给下一个中间件；m2();
+    console.log("m1end");
+}
+let m2 = function(ctx,next){
+    console.log("m2start");
+    next();
+    console.log("m2end");
+}
+app.use(m1);
+app.use(m2);
+app.use(ctx=>{
+    ctx.body = "hello world 你好";
+})
+app.listen(3000);
