3ec528015bedfa84bb0170aa285fdeefbbc07a5f
6xd <ldtheended@163.com>
2020-10-09 16:35:38 +0800

    a1.10（后台页面——渲染admin主页）

--- newsAdmin01/routers/adminRouter.js
+++ newsAdmin01/routers/adminRouter.js
@@ -1,10 +1,11 @@
 const Router = require("koa-router");
 // admin加上 admin前缀
 let router = new Router({
-    prefix:"/admin"  // 前缀  =>  /admin/index  ... /admin
+    prefix:"/admin"  // /admin/index  ... /admin
 });
-router.get("/index", ctx=>{
-    ctx.body = "admin主页"
+router.get("/index",async ctx=>{
+    // 渲染主页
+    // ctx.body = "后端管理的主页"
+    await ctx.render("admin/admin");
 });
-
 module.exports = router;
